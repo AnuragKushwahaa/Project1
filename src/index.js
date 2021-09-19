36baf0e50e454c14dfe0fdbacad73b4e2e6ca1d5
AnuragKushwahaa <90984920+AnuragKushwahaa@users.noreply.github.com>
2021-09-20 02:15:52 +0530

    aa

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
